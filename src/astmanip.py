ecf0762bf61ddb0e9e5d6db92b887775b3cfdb66
aschults <aschults@gmx.net>
2015-04-29 07:36:05 +0200

    Docs + Unit Test

--- src/astmanip.py
+++ src/astmanip.py
@@ -15,6 +15,24 @@ import ast
 from compiler.ast import Node
 
 #
+#
+#   if x==0:
+#      ...
+#   else:
+#      y=1/x
+#
+#
+#   f(z3.Int('_X'))
+# ....
+#   if(x==0):  #== z3.Expr('_X==0')
+#  -->
+#   if(cond_context.wrap_condition(label,x==0):
+#
+# ...
+#
+#        x=x+1  #== z3.Expr('_X+1')
+#             if cond_context.wrap_condition(label2,x>25):  #== z3.Expr('_X+1>25')
+#
 # expects global var cond_context: class with methods 
 #    instrument(self,refNo,marker[[e.g. if endif else endelse]])
 #    wrap_condition(self,refNo,condition,refToOuterIf)
@@ -22,72 +40,102 @@ from compiler.ast import Node
 #  Use  instance.visit(astTree) to start adding instrumentation
 #
 class InstrumentingVisitor(ast.NodeTransformer):
+    """
+    AST Node transformer that inserts instrumentation into the tree.
+     * All instrumentation is implemented as method call on variable "cond_context". 
+     * If conditions are wrapped by "cond_context.wrap_condition(<<ReferenceId>>,<<original condition>>,<<OuterIfId>>)
+       * ReferenceId: Each wrapped condition is associated with an id (counting upward from 0).
+       * original condition: AST tree of the original condition
+       * OuterIfID: In case of nested ifs: The ReferenceID of the next ancestor IF in the tree
+     * Additionally transforms unknown_int and unknown_choice into a method call and adds reference IDs:
+       --> cond_context.unknown_choice(<<ReferenceId>>,<<original args>>)
+       --> cond_context.unknown_int(<<ReferenceId>>
+       
+    TODOS:
+    * Currently same reference ID or even same class for unknowns and if instrumentation --> separate
+    """
+    
     def __init__(self):
         self.refCnt=0
+        self.refCntUnknowns=0
         self.outer=-1
-        self.unknowns={}
+        self._unknowns={}
 
     ctx_var=ast.Name(id='cond_context',ctx=ast.Load())
     
-    # Generate the instrumentation call for AST
-    def gen_instr_call(self,s):
-        mark=ast.Str(s=s)
-        func=ast.Attribute(value=InstrumentingVisitor.ctx_var,attr='instrument',ctx=ast.Load())
-        return ast.Call(func=func,args=[ast.Num(n=self.refCnt),mark],keywords=[])
-
-    # Generate the condition wrapper call. t is the AST expression tree for the conditional
+    @property
+    def refLength(self):
+        """Contains the last ReferenceId used for IFs +1 (array length semantics)"""
+        return self.refCnt
+    
+    @property
+    def unknowns(self):
+        """returns a map unknownId --> corresponding Call node in the AST tree"""
+        return self._unknowns
+    
     def gen_wrap_call(self,t):
+        """Generate the condition wrapper call. t is the AST expression tree for the conditional"""
         func=ast.Attribute(value=InstrumentingVisitor.ctx_var,attr='wrap_condition',ctx=ast.Load())
         return ast.Call(func=func,args=[ast.Num(n=self.refCnt),t,ast.Num(n=self.outer)],keywords=[])
         
     def generic_visit(self, node):
+        """ 
+           Override the visitor's generic method to find If conditions and Calls in the tree
+           TODO: better override visit_If and visit_Call instead.
+        """
         #print ast.dump(node)
+        # keep track of nested IFs
         prevOuter=self.outer
         
         if node.__class__.__name__ == 'Call' and node.func.__class__.__name__ == 'Name':
             if node.func.id=='unknown_int':
                 node.func=ast.Attribute(value=InstrumentingVisitor.ctx_var,attr="unknown_int",ctx=ast.Load())
-                node.args.insert(0,ast.Num(n=self.refCnt))
-                self.unknowns[self.refCnt]=node
-                self.refCnt=self.refCnt+1
+                node.args.insert(0,ast.Num(n=self.refCntUnknowns))
+                self._unknowns[self.refCntUnknowns]=node
+                self.refCntUnknowns+=1
             elif node.func.id=='unknown_choice': 
                 node.func=ast.Attribute(value=InstrumentingVisitor.ctx_var,attr="unknown_choice",ctx=ast.Load())
-                node.args.insert(0,ast.Num(n=self.refCnt))
-                self.unknowns[self.refCnt]=node
-                self.refCnt=self.refCnt+1
+                node.args.insert(0,ast.Num(n=self.refCntUnknowns))
+                self._unknowns[self.refCntUnknowns]=node
+                self.refCntUnknowns+=1
         elif node.__class__.__name__ == 'If' :
             #print ast.dump(node.test)
             t=node.test
             node.test=self.gen_wrap_call(t)
-            #node.body.insert(0,ast.Expr(value=self.gen_instr_call("if")))
-            #node.body.append(ast.Expr(value=self.gen_instr_call("endif")))
-            # NOTE: elif are expanded to If(..., orelse=) after AST
-            #if node.orelse:
-            #    node.orelse.insert(0,ast.Expr(value=self.gen_instr_call("else")))
-            #    node.orelse.append(ast.Expr(value=self.gen_instr_call("endelse")))
-                
             self.outer=self.refCnt
             self.refCnt=self.refCnt+1
 
         #print node.__class__.__name__
+        # Go through all children... original method would do this already.
         for c in ast.iter_child_nodes(node):
             self.visit(c)
             
+        # keep track of nested IFs
         self.outer=prevOuter
         return node
 
 
-
-
-
-
-
 class TemplateTransformer(ast.NodeTransformer):
+    """
+    Replaces unknown_int and unknown_choice with the supplied values
+    Requires that the visited AST tree has been instrumented (using InstrumentingVisitor) as the
+    replacements are matched by ReferenceId.
+    """
+    
     def __init__(self,unknown_vars, unknown_choices):
+        """
+        Constructor. Requires two maps to replace unknown_ints and unknown_choices:
+         * unknown_ints: ReferenceId -> integer literal to replace with
+         * unknwon_choice: ReferenceId -> argument of the function to substitute the call with
+           e.g. cond_context.unknown_choice(4,v0,v1,v2) ( {4:1} ) --> replace by "v1" in AST
+        """
         self.unknown_vars=unknown_vars
         self.unknown_choices=unknown_choices
 
     def visit_Call(self, node):
+        """
+        called from generic_visitor during visit for all calls. only reacts on unknown_... methods
+        """
         rv=node
         if node.func.attr=='unknown_int':
             ref=node.args[0].n
---
+++ test/test_astmanip.py
@@ -0,0 +1,78 @@
+'''
+Created on 29.04.2015
+
+@author: schultschik
+'''
+import unittest
+import astmanip
+import ast
+
+
+def compileFkt(astTree,context,fname):
+    ast.fix_missing_locations(astTree)
+    comp=compile(astTree, "<no>", "exec")
+    sc={"cond_context":context}
+    sc2={}
+    exec(comp,sc,sc2)
+    #spec=inspect.getargspec(sc2[fname])
+    return sc2[fname]
+
+    
+class Test(unittest.TestCase):
+
+    data_wrap="""
+def f(a,b):
+    if a:
+        pass
+    else:
+        pass
+    """
+
+    data_tpl="""
+def f(a,b):
+    if a==cond_context.unknown_choice(123,101,102,103,104):
+        return 1111
+    else:
+        return cond_context.unknown_int(321)
+    """
+
+    
+    class TestContext:
+        def __init__(self,tc,expected_id,expected_cond_val, expected_outer,rv={}):
+            self.tc=tc
+            self.expected_id=expected_id
+            self.expected_cond_val=expected_cond_val
+            self.expected_outer=expected_outer
+            self.wrapCalled=0
+            self.rv=rv
+        def wrap_condition(self,ref,cond_in,outer):
+            self.wrapCalled+=1
+            self.tc.assertEqual(self.expected_id,ref)
+            self.tc.assertEqual(self.expected_cond_val,cond_in)
+            self.tc.assertEqual(self.expected_outer,outer)
+            if self.rv.has_key(ref):
+                return self.rv[ref]
+            else:
+                return cond_in
+            
+
+    def testInstrument(self):
+        visitor=astmanip.InstrumentingVisitor()
+        tree=ast.parse(self.data_wrap)
+        visitor.visit(tree)
+        ctx=self.TestContext(self,0,1,-1)
+        f=compileFkt(tree, ctx, "f")
+        f.__call__(1,0)
+        self.assertEqual(1,ctx.wrapCalled)
+
+    def testTemplate(self):
+        visitor=astmanip.TemplateTransformer({321:888}, {123:2})
+        tree=ast.parse(self.data_tpl)
+        visitor.visit(tree)
+        f=compileFkt(tree, None, "f")
+        self.assertEqual(1111,f.__call__(103,0))
+        self.assertEqual(888,f.__call__(102,0))
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
