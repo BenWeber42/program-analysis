0610d5473836b96702a40eb24336a40abaab4030
Benjamin Weber <dev.ben.weber@gmail.com>
2015-05-15 19:47:35 +0200

    Finished first working version of the data generator (untested as of yet, testing to follow)

--- datagenerator.py
+++ datagenerator.py
@@ -1,27 +1,114 @@
 #!/usr/bin/env python
 
 from sys import argv
-from pysyn import FunctionLoader, FunctionAnalyzer
+from pysyn import FunctionLoader, FunctionAnalyzer, Path
+from math import ceil
+import z3
+
+
+class PathDataGenerator:
+    
+    """
+    Generates input/output samples for a given path
+    """
+    
+    def __init__(self, path):
+        self.path = path
+
+        self.solver = z3.Solver()
+        self.solver.add(path.constraints)
+        self.solver.add(path.relation)
+        self.solver.push()
+        
+        self.stack_size = 0
+    
+    
+    def another(self):
+        
+        sat = self.solver.check()
+        if sat != z3.sat:
+            return None
+    
+        model = self.solver.model()
+
+        in_vector = [model[x] for x in self.path.input]
+        out_vector = [model[y] for y in self.path.output]
+
+        # make sure we don't generate the same sample twice
+        self.solver.add(z3.Not(z3.And(*[ x == model[x] for x in self.path.input])))
+        self.solver.add(z3.Not(z3.And(*[ y == model[y] for y in self.path.output])))
+
+        self.solver.push()
+        self.stack_size += 1
+        
+        return (in_vector, out_vector)
+
+    
+    def take(self, num):
+        v = []
+        for i in xrange(num):
+            d = self.another()
+
+            if d == None:
+                return v
+
+            v.append(d)
+
+        return v
+    
+    def reset(self):
+        while self.stack_size > 0:
+            self.solver.pop()
+            self.stack_size -= 1
+
+
+def print_usage():
+    print("Usage: %s [--help] [-n <X>] [file]" % argv[0])
+    print("")
+    print("   --help   prints this help message and exits.")
+    print("")
+    print("   -n <X>   Tries to generate at least X different testsets evenly distributed")
+    print("            over the paths (default 1000)")
+    print("")
+    print("   [file]   Creates a dataset of output values that achieves 100% path coverage")
+    print("            for a python function 'f' in [file].")
+    print("")
+    exit(0)
 
 
 if __name__ == "__main__":
-    if len(argv) <= 1 or "--help" in argv:
-        print("Usage: %s [--help] [file]" % argv[0])
-        print("")
-        print("   --help   prints this help message and exits.")
-        print("   [file]   Creates a dataset of output values that achieves 100% path coverage")
-        print("            for a python function 'f' in [file].")
+    if "--help" in argv:
+        print_usage()
     
-    else:
-        print("Analyzing file '%s'" % argv[1])
-
-        f = FunctionLoader(argv[1]).get_f()
+    n = 1000
+    
+    if "-n" in argv:
+        index = argv.index("-n")
         
-        analyzer = FunctionAnalyzer(f)
-        analyzer.analyze()
+        if len(argv) <= index + 1:
+            print_usage()
         
-        print("Found %s path(s)" % len(analyzer.paths))
-        if analyzer.is_irreversible:
-            print(" - is irreversible")
-        elif analyzer.migh_be_irreversible:
-            print(" - might be irreversible")
+        n = str(argv[index + 1])
+        argv = argv[:index] + argv[index + 2:]
+    
+    if len(argv) <= 1:
+        print_usage()
+    
+    f = FunctionLoader(argv[1]).get_f()
+        
+    analyzer = FunctionAnalyzer(f)
+    analyzer.analyze()
+    
+    paths = analyzer.paths
+    
+    if len(paths) == 0:
+        print("Warning: No valid paths were found!")
+        exit(-1)
+        
+    per_path = int(ceil(float(n)/float(len(paths))))
+    
+    for p in paths:
+        d = PathDataGenerator(p).take(per_path)
+        
+        for v_in, v_out in d:
+            print(" ".join(map(str, v_out)))
