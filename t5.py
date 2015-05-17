800a48b9240b971325da1c405f021504a6d2091b
aschults <aschults@gmx.net>
2015-05-17 19:16:38 +0200

    test case 5

---
+++ t5.py
@@ -0,0 +1,14 @@
+# this example can contain a zero division, but is otherwise completely reversible
+
+def f(x,y):
+    return x*y,x+440,y
+
+
+def f_inv(p,x,y):
+    dlt=unknown_choice(unknown_choice(y,2*unknown_int()),22)
+    x2=unknown_choice(unknown_choice(x,unknown_choice(5*y)),2*x)
+    
+    if x2 == dlt:
+        return x-unknown_int(),y
+    return x-(100*unknown_int()+10*unknown_int()),unknown_choice(unknown_int(),p)/(x-unknown_int())
+    
---
+++ t5.ref.py
@@ -0,0 +1,8 @@
+# proven correct by brute-force
+def f_inv(p, x, y):
+    dlt = (2) * (220)
+    x2 = x
+    if ((x2) == (dlt)):
+        return (x) - (440), y
+    return (x) - (((100) * (0)) + ((10) * (44))), (p) / ((x) - (440))
+
