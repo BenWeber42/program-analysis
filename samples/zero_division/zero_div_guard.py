c4b98704163772db1380f81f15db97e8dc5d46a3
aschults <aschults@gmx.net>
2015-05-17 18:55:27 +0200

    updated tc

--- samples/zero_division/zero_div_guard.py
+++ samples/zero_division/zero_div_guard.py
@@ -1,20 +1,14 @@
 # this example can contain a zero division, but is otherwise completely reversible
 
 def f(x,y):
-    return x*y,x+333,y
+    return x*y,x+440,y
 
 
 def f_inv(p,x,y):
-    dlt=unknown_int()
+    dlt=unknown_choice(unknown_choice(y,2*unknown_int()),22)
+    x2=unknown_choice(unknown_choice(x,unknown_choice(5*y)),2*x)
     
-    if p>100:
-        if x == unknown_int():
-            return x-dlt,y
-        else:
-            b=1
-            return x-dlt,p/(x-dlt)
-    else:
-        if x == unknown_int():
-            return x-unknown_int(),y
-        return x-unknown_int(),p/(x-unknown_int())
+    if x2 == dlt:
+        return x-unknown_int(),y
+    return x-(100*unknown_int()+10*unknown_int()),unknown_choice(unknown_int(),p)/(x-unknown_int())
     
--- samples/zero_division/zero_div_guard.ref.py
+++ samples/zero_division/zero_div_guard.ref.py
@@ -1,13 +1,8 @@
 # proven correct by brute-force
 def f_inv(p, x, y):
-    dlt = 333
-    if ((p) > (100)):
-        if ((x) == (300)):
-            return (x) - (dlt), y
-        else:
-            b = 1
-            return (x) - (dlt), (p) / ((x) - (dlt))
-    else:
-        if ((x) == (333)):
-            return (x) - (333), y
-        return (x) - (333), (p) / ((x) - (333))
+    dlt = (2) * (220)
+    x2 = x
+    if ((x2) == (dlt)):
+        return (x) - (440), y
+    return (x) - (((100) * (0)) + ((10) * (44))), (p) / ((x) - (440))
+
